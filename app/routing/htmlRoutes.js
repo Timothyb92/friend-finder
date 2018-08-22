ccc2f4feab406224d31d48ee2ec9112ecbedab64
Timothy Bedsole <timbedsole@gmail.com>
2018-08-21 21:09:28 -0400

    exported html routes and requried them in server.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+module.exports = function (app){
+   app.get("/", function(req, res){
+       res.sendFile(path.join(__dirname + "/../public/home.html"))
+   });
+
+   app.get("/survey", function(req, res){
+       res.sendFile(path.join(__dirname + "/../public/survey.html"))
+   });
+}
+// app.get("/", function(req, res){
+//     fs.readFile(__dirname + "/../public/home.html", function(err, data){
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(data);
+//     });
+// })
+
+// app.get("/survey", function(req, res){
+//     fs.readFile(__dirname + "/../public/survey.html", function(err, data){
+//         res.writeHead(200, { "Content-Type": "text/html"});
+//         res.end(data);
+//     })
+// })
+
+// app.listen(PORT, function(){
+//     console.log("Listening on port " + PORT)
+// })
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 var fs = require("fs");
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", function(req, res){
-    fs.readFile(__dirname + "/app/public/home.html", function(err, data){
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });
-})
+// require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// app.get("/", function(req, res){
+//     fs.readFile(__dirname + "/app/public/home.html", function(err, data){
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(data);
+//     });
+// })
 
 app.listen(PORT, function(){
     console.log("Listening on port " + PORT)
