62d688478a93428b90a6f26c64032b5e80210456
Timothy Bedsole <timbedsole@gmail.com>
2018-08-22 20:53:15 -0400

    Added static data to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArr = [
     {
-        "name": "Tim",
-        "photo": "https://avatars0.githubusercontent.com/u/23228051?s=460&v=4",
+        "name": "Ryu",
+        "photo": "https://game.capcom.com/cfn/sfv/as/common/character/bustup/l/ryu.png?h=51778c13c01235d62adfa5eb90016638",
         "scores": [
             5,
             1,
@@ -13,6 +13,70 @@ var friendsArr = [
             5,
             4,
             1
+        ]
+    },
+    {
+        "name": "Ken",
+        "photo": "https://game.capcom.com/cfn/sfv/as/common/character/bustup/l/ken.png?h=c4725797e7d123403e8964d2698998c9",
+        "scores": [
+            4,
+            2,
+            5,
+            5,
+            1,
+            3,
+            4,
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "Chun-Li",
+        "photo": "https://game.capcom.com/cfn/sfv/as/common/character/bustup/l/cnl.png?h=cdcdad24aed42606eede35add77e6d11",
+        "scores":  [
+            2,
+            4,
+            2,
+            5,
+            5,
+            1,
+            5,
+            2,
+            1,
+            3
+        ]
+    },
+    {
+        "name": "Dhalsim",
+        "photo": "https://game.capcom.com/cfn/sfv/as/common/character/bustup/l/dsm.png?h=831bf42208028686d3edefc248f28bdb",
+        "scores": [
+            5,
+            4,
+            3,
+            3,
+            4,
+            1,
+            1,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Ibuki",
+        "photo": "https://game.capcom.com/cfn/sfv/as/common/character/bustup/l/ibk.png?h=e2d46e7b998a37e43d7e511c440c5bff",
+        "scores": [
+            2,
+            3,
+            2,
+            5,
+            4,
+            1,
+            1,
+            5,
+            3,
+            3
         ]
     }
 ]
